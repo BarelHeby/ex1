5a41e06aff29ad94dcf07c4179d103b8694b8146
BarelHeby <barelheby03@gmail.com>
2024-06-02 12:54:48 +0300

    lower loop works great

--- formulas/formula2.s
+++ formulas/formula2.s
@@ -14,53 +14,7 @@ formula2:
     // save args in stack
     pushq   %rdi
     pushq   %rsi
-    pushq   %rdx
-    // zero out xmm0
-    pxor   %xmm0, %xmm0
-
-.upper_loop:
-    cmp    $0, %rdx
-    je .end_upper_loop
-    movaps (%rdi), %xmm1
-    movaps (%rsi), %xmm2
-
-    // a[i] * b[i]
-    mulps %xmm2, %xmm1
-    // sum += a[i] * b[i]
-    addps %xmm1, %xmm0  
-
-    // move to next element
-    addq $16, %rdi 
-    // move to next element
-    addq $16, %rsi 
-    // decrement counter
-    sub  $4, %rdx 
-
-    jmp .upper_loop
-.end_upper_loop:
-    // movaps %xmm0, %xmm1
-    // shufps $0xB1, %xmm0, %xmm0
-    // addps %xmm0, %xmm1
-
-    // movaps %xmm1, %xmm0
-    // shufps $0x01, %xmm0, %xmm0
-    // addps %xmm1, %xmm0
-    // xmm0 = [x1,x2,x3,x4]
-    // xmm1 = [x2,x3,x4,x1]
-    // xmm2 = [x3,x4,x1,x2]
-    // xmm3 = [x4,x1,x2,x3]
-    movaps %xmm0, %xmm1
-    pshufd $0x39, %xmm1, %xmm1  
-
-    movaps %xmm1,%xmm2
-    pshufd $0x39, %xmm2, %xmm2  
-
-    movaps %xmm2,%xmm3
-    pshufd $0x39, %xmm3, %xmm3  
-
-    addps %xmm1, %xmm0
-    addps %xmm2, %xmm0
-    addps %xmm3, %xmm0
+    pushq   %rdx 
 
 .second_part:
     // result in xmm0
@@ -73,11 +27,6 @@ formula2:
     mov $0x3f800000,%eax
     movd %eax, %xmm5
     shufps $0x00, %xmm5, %xmm5 
-
-
-    pop %rdx
-    pop %rsi
-    pop %rdi
     xor %r8, %r8
 // calculate mul(a[i]^2+b[i]^2-2*x[i]*y[i]+1) from i=1 to size
 .lower_loop:
@@ -87,31 +36,17 @@ formula2:
     movaps (%rdi), %xmm2
     movaps (%rsi), %xmm3
 
-    // a[i]^2
-    movaps %xmm2, %xmm4
-    mulps %xmm4, %xmm4
+    // a[i]-b[i]
+    subps %xmm3, %xmm2
 
-    // b[i]^2
-    movaps %xmm3, %xmm6
-    mulps %xmm6, %xmm6
-
-    // a[i]*b[i]
-    movaps %xmm2, %xmm7
-    mulps %xmm3, %xmm7
-
-    // 2*a[i]*b[i]
-    addps %xmm7, %xmm7
-
-    // a[i]^2+b[i]^2-2*a[i]*b[i]
-    addps %xmm4, %xmm6
-    subps %xmm7, %xmm6
+    // (a[i]-b[i])^2
+    mulps %xmm2, %xmm2
 
     // add 1 to the result
-    addps %xmm5, %xmm6
-
+    addps %xmm5, %xmm2
 
     // store the result in xmm3
-    movaps %xmm6, %xmm3
+    movaps %xmm2, %xmm3
 
     cmp $0, %r8
     je .first
@@ -147,9 +82,51 @@ formula2:
     mulps %xmm2, %xmm1
     mulps %xmm3, %xmm1
     mulps %xmm4, %xmm1
-.end_equistion:
+
+.sum_totals:
+    pop %rdx
+    pop %rsi
+    pop %rdi
+    pxor %xmm4, %xmm4
+.lp:
+    cmp $0, %rdx
+    je .end_totals
+    movaps (%rdi), %xmm2
+    movaps (%rsi), %xmm3
+
+    // a[i] * b[i]
+    mulps %xmm3, %xmm2
+    
+    divps %xmm2, %xmm1
+
+    addps %xmm2, %xmm4
+
+    // move to next element
+    addq $16, %rdi
+    // move to next element
+    addq $16, %rsi
+    // decrement counter
+    sub $4, %rdx
+    jmp .lp
+
+.end_totals:
+    movaps %xmm4, %xmm2
+    pshufd $0x39, %xmm2, %xmm2
+
+    movaps %xmm2, %xmm3
+    pshufd $0x39, %xmm3, %xmm3
+
+    movaps %xmm3, %xmm5
+    pshufd $0x39, %xmm5, %xmm5
+
+    addps %xmm2, %xmm4
+    addps %xmm3, %xmm4
+    addps %xmm5, %xmm4
+    // return value
+    movaps %xmm4, %xmm0
+// .end_equistion:
     // divide xmm0 by xmm1
-    divps %xmm0,%xmm1 
+    // divps %xmm0,%xmm1 
     // movaps %xmm1, %xmm0
     // movaps %xmm1, %xmm0
 
